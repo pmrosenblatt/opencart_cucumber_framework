cad4d0f3f4da036ca6a8b2b4b9064cd579e31e28
paige <paigerosenblatt2004@gmail.com>
2023-09-11 21:55:58 -0500

    first commit

---
+++ src/test/java/opencart_cucumber/opencart_cucumber_framework/AppTest.java
@@ -0,0 +1,20 @@
+package opencart_cucumber.opencart_cucumber_framework;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
